b131fccabc7c77fa7855f58b1fb08b5093343511
Andrey Flek <a.flek@mail.ru>
2020-09-02 11:06:44 +0300

    package apiserver imported to main.go

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+	"github.com/aflek/http-rest-api/internal/app/apiserver"
+)
+
 func main() {
-  
+  //Создаем API-сервер
+  s := apiserver.New()
+  //Запускаем сервер
+  if err:= s.Start; err != nil {
+    log.Fatal(err)
+  }
 }
